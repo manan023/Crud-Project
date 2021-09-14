c58b758fa6470979ad44e1ec49d2a4cc53c1c5cb
Cyber <mananarora023@gmail.com>
2021-09-14 23:33:00 +0530

    Add Java Script , add function on Load Data button , fix some errors in html and css

---
+++ Script.js
@@ -0,0 +1,12 @@
+
+function myFunction(){
+    var a = document.getElementById("refresh-data");
+    if (a.style.display === "none") {
+        a.style.display = "block";
+    } else {
+        a.style.display = "none";
+    }
+};
+
+
+
