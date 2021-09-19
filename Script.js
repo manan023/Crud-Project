fcd4602fe2f390a794d047ac4254ccf4c9222315
Cyber <mananarora023@gmail.com>
2021-09-19 22:28:24 +0530

    Learning how to create Table using for loop

--- Script.js
+++ Script.js
@@ -1,11 +1,30 @@
 
 function myFunction(){
-    var a = document.getElementById("refresh-data");
+    var a = document.getElementsByClassName("refresh-data")[2];
     if (a.style.display === "none") {
         a.style.display = "block";
     } else {
         a.style.display = "none";
     }
+};
+
+
+// Learning For loop 
+
+var user = [
+    {
+        firstName: "Harsh",
+        midleName: " ",
+        lastnmae: "Kurra",
+        email: "hk@gmail.com",
+        role: "Employe",
+        phone: "807745546",
+        address: "Chandigarah"
+    }
+];
+
+for(var i=0; i<user.length; i++){
+console.log(user[i].firstName);
 };
 
 
