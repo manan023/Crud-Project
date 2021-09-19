4c53cc77bb4333a5f5eef7c0df925bee64c371e4
Cyber <mananarora023@gmail.com>
2021-09-20 01:38:53 +0530

    Run all values of table in console

--- Script.js
+++ Script.js
@@ -13,19 +13,37 @@ function myFunction(){
 
 var user = [
     {
-        firstName: "Harsh",
-        midleName: " ",
-        lastnmae: "Kurra",
-        email: "hk@gmail.com",
-        role: "Employe",
-        phone: "807745546",
-        address: "Chandigarah"
+        firstName: "Harsh",midleName: " ",lastName: "Kurra",email: "hk@gmail.com",role: "Manager",phone: "807745546",address: "Chandigarah"
+    },
+    {
+        firstName: "Manan",midleName: " ",lastName: "Arora",email: "M@gmail.com",role: "Employe",phone: "807745546",address: "Chandigarah"
+    },
+    {
+        firstName: "Manish",midleName: "Kumar",lastName: "Singla",email: "ms@gmail.com",role: "Super Admin",phone: "807745546",address: "Chandigarah"
+    },
+    {
+        firstName: "Rohit ",midleName: "kumar",lastName: "singla",email: "r@gmail.com",role: "Suscriber",phone: "807745546",address: "Chandigarah"
+    },
+    {
+        firstName: "Rockey",midleName: " ",lastName: "singla",email: "rs@gmail.com",role: "client",phone: "807745546",address: "Chandigarah"
+    },
+    {
+        firstName: "Tom",midleName: "kumar",lastName: "singla",email: "ts@gmail.com",role: "Amin",phone: "807745546",address: "Chandigarah"
+    },
+    {
+        firstName: "Harry",midleName: "kumar",lastName: "singla",email: "hs@gmail.com",role: "QA",phone: "807745546",address: "Chandigarah"
     }
 ];
 
 for(var i=0; i<user.length; i++){
 console.log(user[i].firstName);
 };
+
+
+var table = 10
+for(var i = 0; i<=table; i = i+2){
+    console.log("table");
+}
 
 
 
