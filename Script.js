8a5f5d848493334ced6f47c241011526bd0b6d4f
Cyber <mananarora023@gmail.com>
2021-09-30 00:34:47 +0530

    Add Edit or Dlt Btn but its not work

--- Script.js
+++ Script.js
@@ -64,18 +64,39 @@ window.addEventListener('DOMContentLoaded', (event) => {    // use this bcz this
         var tableRow = document.createElement("tr");
         tableRow.className = "row";
 
-        for(var j = 0; j < userInfo.length; j++){
+        for(var j = 0; j < userInfo.length-1; j++){
             var column = document.createElement("td");
             column.className = "col";
-
-
-
             column.innerText = user[i][userInfo[j]];
-
-
             tableRow.appendChild(column);
             tableBody.appendChild(tableRow);
         }
+
+        // Edit btn
+        var colEdit = document.createElement("td");
+        var edtBtn = document.createElement("button"); 
+        edtBtn.className = "btn"
+        edtBtn.innerText = "Edit";
+        // edtBtn.addEventListener('click',function(){
+        //     let html = column.innerHTML;
+        //     column.innerHTML = <textarea class="form-control" class="text-area" id = "text-area">${html}</textarea>
+
+        // });
+
+        // delete btn 
+        var dltBtn = document.createElement("button");
+        dltBtn.className = "btn"
+        dltBtn.innerText = "Dlt";
+
+
+        tableRow.appendChild(colEdit);
+        colEdit.appendChild(edtBtn);
+        colEdit.appendChild(dltBtn);
+
+
+        
+
+        
       };
 });
 
@@ -85,25 +106,25 @@ window.addEventListener('DOMContentLoaded', (event) => {    // use this bcz this
 
 var user = [
     {
-        firstName: "Harsh",middleName: " ",lastName: "Kurra",email: "hk@gmail.com",role: "Manager",phone: "807745546",address: "Chandigarah", link: "edit dlt"
+        firstName: "Harsh",middleName: " ",lastName: "Kurra",email: "hk@gmail.com",role: "Manager",phone: "807745546",address: "Chandigarah"
     },
     {
-        firstName: "Manan",middleName: " ",lastName: "Arora",email: "M@gmail.com",role: "Employe",phone: "807745546",address: "Chandigarah", link: "edit dlt"
+        firstName: "Manan",middleName: " ",lastName: "Arora",email: "M@gmail.com",role: "Employe",phone: "807745546",address: "Chandigarah"
     },
     {
-        firstName: "Manish",middleName: "Kumar",lastName: "Singla",email: "ms@gmail.com",role: "Super Admin",phone: "807745546",address: "Chandigarah", link: "edit dlt"
+        firstName: "Manish",middleName: "Kumar",lastName: "Singla",email: "ms@gmail.com",role: "Super Admin",phone: "807745546",address: "Chandigarah"
     },
     {
-        firstName: "Rohit ",middleName: "kumar",lastName: "singla",email: "r@gmail.com",role: "Suscriber",phone: "807745546",address: "Chandigarah", link: "edit dlt"
+        firstName: "Rohit ",middleName: "kumar",lastName: "singla",email: "r@gmail.com",role: "Suscriber",phone: "807745546",address: "Chandigarah"
     },
     {
-        firstName: "Rockey",middleName: " ",lastName: "singla",email: "rs@gmail.com",role: "client",phone: "807745546",address: "Chandigarah", link: "edit dlt"
+        firstName: "Rockey",middleName: " ",lastName: "singla",email: "rs@gmail.com",role: "client",phone: "807745546",address: "Chandigarah"
     },
     {
-        firstName: "Tom",middleName: "kumar",lastName: "singla",email: "ts@gmail.com",role: "Amin",phone: "807745546",address: "Chandigarah", link: "edit dlt"
+        firstName: "Tom",middleName: "kumar",lastName: "singla",email: "ts@gmail.com",role: "Amin",phone: "807745546",address: "Chandigarah"
     },
     {
-        firstName: "Harry",middleName: "kumar",lastName: "singla",email: "hs@gmail.com",role: "QA",phone: "807745546",address: "Chandigarah", link: "edit dlt"
+        firstName: "Harry",middleName: "kumar",lastName: "singla",email: "hs@gmail.com",role: "QA",phone: "807745546",address: "Chandigarah"
     }
 ];
 
