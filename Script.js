0330a92aba7622439f0d63545dae1c7a851bdcdf
Cyber <mananarora023@gmail.com>
2021-10-05 01:44:53 +0530

    Editable row using Edit button

--- Script.js
+++ Script.js
@@ -1,5 +1,6 @@
         //   Hide or unHide
-
+window.editState ={};
+window.dltState = {};
 function myFunction(){
     var a = document.getElementById("refresh-data");
     console.log("ok");
@@ -9,106 +10,96 @@ function myFunction(){
     else {
         a.style.display = "none";
     }
-
-    
 };
 
-window.addEventListener('DOMContentLoaded', (event) => {    // use this bcz this not in function when i put this code in function its #work
+function emptyTable() {
+    var tableBody = document.getElementById("table-body");
+    tableBody.innerHTML = "";
+  };
+
+
+    //    Create table
+
+function createTable(){
     for( let i=0; i<user.length; i++){
         console.log("Manan");
-    
-
-                // FIRST METHOD 
-
-        // var tableBody = document.getElementById("table-body");
-        // var tableRow  = document.createElement("tr");
-        // tableRow.className= "row";
-    
-        // var firstNameCol = document.createElement("td");
-        // firstNameCol.innerText = user[i].firstName;
-    
-        // var middleNameCol = document.createElement("td");
-        // middleNameCol.innerText = user[i].middleName;
-    
-        // var lastNameCol = document.createElement("td");
-        // lastNameCol.innerText = user[i].lastName;
-
-        // var emailCol = document.createElement("td");
-        // emailCol.innerText = user[i].email;
-
-        // var roleCol = document.createElement("td");
-        // roleCol.innerText = user[i].role;
-
-        // var phoneCol = document.createElement("td");
-        // phoneCol.innerText = user[i].phone;
-
-        // var addressCol = document.createElement("td");
-        // addressCol.innerText = user[i].address;
-    
         
-        // tableRow.appendChild(firstNameCol);
-        // tableRow.appendChild(middleNameCol);
-        // tableRow.appendChild(lastNameCol);
-        // tableRow.appendChild(emailCol);
-        // tableRow.appendChild(roleCol);
-        // tableRow.appendChild(phoneCol);
-        // tableRow.appendChild(addressCol);
-        // tableBody.append(tableRow);
-        
-    
-        // console.log(user[i].firstName);
-
-                // SECOND METHOD
-
         var tableBody = document.getElementById("table-body");
         var tableRow = document.createElement("tr");
         tableRow.className = "row";
         tableRow.setAttribute("id","body-row");
-
-        for(var j = 0; j < userInfo.length; j++){
+        
+        for (var j = 0; j < userInfo.length; j++) {
             var column = document.createElement("td");
             column.className = "col";
-            column.innerText = user[i][userInfo[j]];
+            if (editState[i]) {
+              column.innerHTML = "<input class='input' value=" + user[i][userInfo[j]] + ">";
+            } 
+            else if(dltState[i]){
+                column.innerHTML = "<input class= 'input'> "
+            }
+            else {
+              column.innerText = user[i][userInfo[j]];
+            }
             tableRow.appendChild(column);
             tableBody.appendChild(tableRow);
-        };
+          }
+
+
 
             // Edit Button
             var colEdit = document.createElement("td");
             var edtbtn = document.createElement("button");
             edtbtn.className = "btn"
-            edtbtn.innerText = "Edit"
+            if(editState[i]) {
+                edtbtn.innerText = "save"
+            }else{
+                edtbtn.innerText = "Edit"
+            }
             edtbtn.addEventListener('click', ()=>{
                 console.log("row" + " " + i);
-                column.innerText= "ok"    
+                emptyTable();
+                if (editState[i]) {
+                    editState={};
+                }
+                else {
+                    editState={};
+                    editState[i] = true;
+                }
+                createTable();
             });
 
-
-
             
-
             // Delete button
             var dltbtn = document.createElement("button");
             dltbtn.className = "btn"
             dltbtn.innerText = "Delete"
+            dltbtn.addEventListener('click', ()=>{
+                console.log("dlt" + i);
+                emptyTable();
+                dltState = {};
+                dltState[i] = true;
+                createTable();
+            });
 
-            tableRow.appendChild(colEdit);
-            colEdit.appendChild(edtbtn);
-            colEdit.appendChild(dltbtn);
-            }
-
-        });
+        
+        
 
 
 
+        tableRow.appendChild(colEdit);
+        colEdit.appendChild(edtbtn);
+        colEdit.appendChild(dltbtn);
+    };
+};
 
-                            // TRY
+    
 
-        // edtBtn.addEventListener('click',function(){
-        //     let html = column.innerHTML;
-        //     column.innerHTML = <textarea class="form-control" class="text-area" id = "text-area">${html}</textarea>
 
-        // });
+window.addEventListener('DOMContentLoaded', (event) => {
+    createTable()
+});
+
 
 
 
